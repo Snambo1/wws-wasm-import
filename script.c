4ca2274e6e0a179c1a168201b96199c57622a78a
Nathan Stiles <stiles.nathan@outlook.com>
2022-08-25 00:22:52 -0400

    work

--- script.c
+++ script.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
+extern void someFunction(int i);
 
-int main() {
-    printf("Hello World\n");
-    return 0;
+int add(int length, int* array) {
+    int result = 0;
+	for (int i = 0; i < length; i++) {
+		result += array[i];
+		array[i]++;
+	}
+	return result;
 }
