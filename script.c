47f1f9e6c2988f9db92d05c1fdfbb22647efc14c
Nathan Stiles <stiles.nathan@outlook.com>
2025-01-27 07:47:26 -0500

    working with curl...powershell script may be sketchy...checking...

--- script.c
+++ script.c
@@ -1,8 +1,17 @@
 #include <string.h>
 #include <emscripten.h>
-#include "script.h"
 
 // emcc -O3 -s WASM=1 -s SIDE_MODULE=2 script.c -o script.wasm
+// curl -F "data=@./script.wasm" "https://localhost:7086/api/Wasm/Upload/xg2ndver6ndmwxz9rxne4v2j2la8ild43g505x01or2s0d1l1m"
+// Get-FileHash -Path ".\script.wasm" -Algorithm SHA256
+
+extern void _actr_log(const char* value, int length);
+
+extern void _actr_fillStyle(const char* value, int length);
+extern void _actr_strokeStyle(const char* value, int length);
+
+extern void _actr_fillRect(int x, int y, int w, int h);
+extern void _actr_strokeRect(int x, int y, int w, int h);
 
 void actr_fillStyle(const char* value) {
   _actr_fillStyle(value, strlen(value));
@@ -16,14 +25,12 @@ void actr_log(const char* buffer) {
   _actr_log(buffer, strlen(buffer));
 }
 
-EMSCRIPTEN_KEEPALIVE int square(int value) {
-  return value * value;
-} 
+
 // time in ms
 int main() {
   // static float time = 0;
-  float time = 0;
-  time /= 1000.0;
+  // float time = 0;
+  //time /= 1000.0;
   
 
   actr_fillStyle("black");
@@ -31,7 +38,7 @@ int main() {
   
   actr_fillStyle("red");
   actr_strokeStyle("white");
-  _actr_fillRect(time / 10.0, 20, 30, 40);
-  _actr_strokeRect(time / 10.0, 20, 30, 40);
+  _actr_fillRect(10, 20, 30, 40);
+  _actr_strokeRect(10, 20, 30, 40);
   return 0;
 }
--- script.h
+++ script.h
@@ -3,5 +3,5 @@ extern void _actr_log(const char* value, int length);
 extern void _actr_fillStyle(const char* value, int length);
 extern void _actr_strokeStyle(const char* value, int length);
 
-extern void _actr_fillRect(float x, float y, float w, float h);
-extern void _actr_strokeRect(float x, float y, float w, float h);
+extern void _actr_fillRect(int x, int y, int w, int h);
+extern void _actr_strokeRect(int x, int y, int w, int h);
