781ecac5f8b7527049552eccddfb60fd0952ae3a
Nathan Stiles <stiles.nathan@outlook.com>
2025-01-28 10:21:43 -0500

    paths

--- actrwasm.h
+++ actrwasm.h
@@ -2,15 +2,57 @@
 #ifndef ACTRWASM_H
 #define ACTRWASM_H
 #include <string.h>
+#include <stdio.h>
+
+// externs
 void actr_canvas2d_fillStyle(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 void actr_canvas2d_strokeStyle(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
-void actr_canvas2d_fillRect(float x, float y, float w, float h);
-void actr_canvas2d_strokeRect(float x, float y, float w, float h);
-void actr_canvas2d_fillTextLength(float x, float y, char * text, int length);
 
-void actr_canvas2d_fillText(float x, float y, char *text) {
+void actr_canvas2d_beginPath();
+
+void actr_canvas2d_ellipse(double x, double y, double radiusX, double radiusY, double rotation, double startAngle, double endAngle, char counterclockwise);
+
+void actr_canvas2d_fill();
+void actr_canvas2d_fillRect(double x, double y, double w, double h);
+void actr_canvas2d_fillTextLength(double x, double y, char *text, int length);
+
+void actr_canvas2d_stroke();
+void actr_canvas2d_strokeRect(double x, double y, double w, double h);
+
+void actr_sizeSanity(char *text, int length);
+// end externs
+
+// helpers
+void actr_canvas2d_fillText(double x, double y, char *text)
+{
     actr_canvas2d_fillTextLength(x, y, text, strlen(text));
 }
+// end helpers
 
+struct ActrPoint
+{
+    int x;
+    int y;
+};
+struct ActrState
+{
+    unsigned int structSize;
+    struct ActrPoint canvasSize;
+    struct ActrPoint pointer;
+};
+static struct ActrState state;
+
+void EMSCRIPTEN_KEEPALIVE actr_doSanity()
+{
+    state.canvasSize.x = 0;
+    state.canvasSize.y = 0;
+    state.pointer.x = 0;
+    state.pointer.y = 0;
+
+    int SIZE = 64;
+    char buffer[SIZE];
+    snprintf(buffer, SIZE, ":%lu:%lu:%lu:%lu", sizeof(int), sizeof(double), sizeof(struct ActrState), &state);
+    actr_sizeSanity(buffer, strlen(buffer));
+}
 
 #endif
--- script.c
+++ script.c
@@ -1,35 +1,51 @@
 
 #include <emscripten.h>
+#include <math.h>
 #include "actrwasm.h"
+
+// using SIDE_MODULE may cause some standard function to be culled from the assembly, like strlen and functions from math.h
+// emcc -O3 --no-entry -s WASM=1 -s ENVIRONMENT=web -s ERROR_ON_UNDEFINED_SYMBOLS=0 script.c -o script.wasm
+// vvv NO vvv
 // emcc -O3 --no-entry -s WASM=1 -s ENVIRONMENT=web -s ERROR_ON_UNDEFINED_SYMBOLS=1 -s SIDE_MODULE=2 script.c -o script.wasm
 // emcc -O3 --no-entry -s WASM=1 -s ENVIRONMENT=web -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s SIDE_MODULE=2 script.c -o script.wasm
 // curl -F "data=@./script.wasm" "https://localhost:7086/api/Wasm/Upload/xg2ndver6ndmwxz9rxne4v2j2la8ild43g505x01or2s0d1l1m"
 // Get-FileHash -Path ".\script.wasm" -Algorithm SHA256
 // or use upload.exe to upload
-// or get cli @ 
+// or get cli @
 
+static double time = 0;
 
-
-
-
-static float time = 0;
-
-void EMSCRIPTEN_KEEPALIVE step(float delta) {
-  time += delta * 0.001;
-  // static float time = 0;
-  
-  
-
-  actr_canvas2d_fillStyle(0, 0, 0, 100);
-  actr_canvas2d_fillRect(0, 0, 9999, 9999);
-  
-  actr_canvas2d_fillStyle(255, 0, 0, 100);
-  actr_canvas2d_strokeStyle(255, 255, 255, 100);
-  actr_canvas2d_fillRect(time, 20, 30, 40);
-  actr_canvas2d_strokeRect(time, 20, 30, 40);
-
-  actr_canvas2d_fillStyle(0, 255, 0, 100);
-  actr_canvas2d_fillText(time, 80, "Hello World!");
+void EMSCRIPTEN_KEEPALIVE init()
+{
 
 }
 
+void EMSCRIPTEN_KEEPALIVE tap(double x, double y)
+{
+}
+
+void EMSCRIPTEN_KEEPALIVE step(double delta)
+{
+
+  time += delta * 0.001;
+
+  double x = fmod(time, 100.0);
+
+  actr_canvas2d_fillStyle(0, 0, 0, 100);
+  actr_canvas2d_fillRect(0, 0, 9999, 9999);
+
+  actr_canvas2d_fillStyle(255, 0, 0, 50);
+  actr_canvas2d_strokeStyle(255, 255, 255, 100);
+
+  //  actr_canvas2d_fillRect(x, 10, 60, 15);
+  actr_canvas2d_strokeRect(x, 10, 60, 14);
+
+  actr_canvas2d_fillStyle(0, 200, 200, 100);
+  actr_canvas2d_fillText(x + 5, 20, "Welcome!");
+
+  actr_canvas2d_fillStyle(255, 0, 0, 50);
+  actr_canvas2d_beginPath();
+  actr_canvas2d_ellipse(50, 100, 40, 40, time, 0, fmod(time, 6.28), 0);
+  actr_canvas2d_fill();
+
+}
